eb6e37cedadcf837cacfeab45828b8741b5cac68
PRITAM188 <159116098+PRITAM188@users.noreply.github.com>
2025-08-19 20:42:32 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cors({
     origin: ["https://pritam-exam-portal.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
